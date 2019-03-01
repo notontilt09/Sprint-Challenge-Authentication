d41798fdeff2e0e8fbac7f70f16ba7c102b4cf95
Daniel Weinman <namniewd@gmail.com>
2019-03-01 11:08:52 -0500

    added register user function with hashed pw

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js')
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -9,7 +11,17 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 }
 
 function login(req, res) {
