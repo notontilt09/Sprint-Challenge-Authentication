fe3ff256ff0d86fc1cf44ce54716bb739a17d0fb
Daniel Weinman <namniewd@gmail.com>
2019-03-01 11:36:12 -0500

    crapped out a front-end

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, NavLink, withRouter } from 'react-router-dom'
+import Login from './components/Login/Login.js';
+import Jokes from './components/Jokes/Jokes.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+    
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
